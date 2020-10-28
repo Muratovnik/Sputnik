b6687aadd6511d8eb529e5ebb65a120b0a5bcd21
Muratovnik <68428072+Muratovnik@users.noreply.github.com>
2020-10-28 13:05:15 +0300

    Added scripts for burger

--- js/main.js
+++ js/main.js
@@ -5,5 +5,14 @@ $(function() {
       dots: true,
       arrows: false
     });
+
+
+    // menu toggle  
+  $('.js-menu-toggle').on('click', function() {
+    $(this).toggleClass('is-active');
+    $('.main_header__list').toggleClass('is-open');
   });
+});
   
+
+
